dcf4f325212b18648da800acd90e6db5d93ab9c1
Suchitra Naik <susanaik@gmail.com>
2018-08-12 12:42:48 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  attr_reader: :artist_name
-  # def artist_name
-  # end
+
+  def artist_name
+    self.artist_name
+  end
 
   def artist_name=(name)
     new_song = Song.new
-    new_song.artist_name = name 
-    new_song.save 
+    new_song.artist_name = name
+    new_song.save
     new_song
   end
 end
